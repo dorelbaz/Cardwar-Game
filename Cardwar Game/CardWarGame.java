6ac60731e2cfbee2acb9f5b87cddef6f335caf82
dorelbaz <165473507+dorelbaz@users.noreply.github.com>
2024-03-31 15:19:40 +0300

    Add files via upload

---
+++ Cardwar Game/CardWarGame.java
@@ -0,0 +1,12 @@
+public class CardWarGame
+{
+    public static void main(String[] args)
+    {
+        /* Generates 2 random card decks for player 1 and 2.*/
+        DeckOfCards player1 = new DeckOfCards();
+        DeckOfCards player2 = new DeckOfCards();
+
+        /* Starts the card war game. */
+        player1.versus(player2);
+    }
+}
